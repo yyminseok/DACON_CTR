24482aef3c4d26c023af12587af04eb19504ed94
yyminseok <yms5440@gmail.com>
2024-05-12 00:47:37 +0900

    .

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ class FeaturesLinear(torch.nn.Module):
 
     def __init__(self, field_dims, output_dim=1):
         super().__init__()
-        self.fc = torch.nn.Embedding(sum(field_dims), output_dim).to('cuda:0')
-        self.bias = torch.nn.Parameter(torch.zeros((output_dim,))).to('cuda:0')
+        self.fc = torch.nn.Embedding(sum(field_dims), output_dim)
+        self.bias = torch.nn.Parameter(torch.zeros((output_dim,)))
         self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.float32)
 
     def forward(self, x):
@@ -17,7 +17,7 @@ class FeaturesLinear(torch.nn.Module):
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
         x = x + x.new_tensor(self.offsets).unsqueeze(0)
-        return torch.sum(self.fc(x), dim=1) + self.bias
+        return torch.sum(self.fc(x), dim=1) + self.bias #여기서 오류남
 
 class FeaturesEmbedding(torch.nn.Module):
 
